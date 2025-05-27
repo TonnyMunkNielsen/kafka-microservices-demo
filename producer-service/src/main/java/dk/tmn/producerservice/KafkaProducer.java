b4e80e097aa0fb0023fd1cc714df34553856e720
TonnyMunkNielsen <TonnyMunkNielsen@protonmail.ch>
2025-05-27 23:58:44 +0200

    fix

--- producer-service/src/main/java/dk/tmn/producerservice/KafkaProducer.java
+++ producer-service/src/main/java/dk/tmn/producerservice/KafkaProducer.java
@@ -1,6 +1,5 @@
 package dk.tmn.producerservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
